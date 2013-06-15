330480146ac83349361fd1b355523144f3a0e771
Robin Martens <robin_martens@brown.edu>
2013-06-15 19:00:14 +0200

    quicksort complete

--- src/darray_algos.c
+++ src/darray_algos.c
@@ -49,11 +49,16 @@ int DArray_quicksort(DArray *array, DArray_compare cmp) {
 		void *cur;
 		int i;
 
+		/*
 		for(i = 0, cur = DArray_first(array); 
 			i < DArray_count(array);
 			i++, cur = DArray_get(array, i)) {
+		*/
+
+		for(cur = DArray_pop(array); cur != NULL; cur = DArray_pop(array)) {
 
 			check(cur != NULL, "cur is NULL");
+			debug("length of array: %d; i: %d", DArray_count(array), i);
 
 			debug("comparing cur to pivot");
 			debug("cur: %s", (char *)cur);
@@ -62,15 +67,16 @@ int DArray_quicksort(DArray *array, DArray_compare cmp) {
 			debug("address of comparator: %p", cmp);
 			debug("cmp(&pivot, &cur): %d", cmp(&pivot, &cur));
 
-			if(cmp(&pivot, &cur) < 0){
+			if(cmp(&pivot, &cur) > 0){
 				debug("cur is less than the pivot");
 				DArray_push(less, cur);
 			}else{
 				debug("cur is more than the pivot");
 				DArray_push(more, cur);
 			}
-
 		}
+
+		check(DArray_count(array) == 0, "array not empty");
 
 		// get lengths before
 		int len_less_before = DArray_count(less);
@@ -84,26 +90,29 @@ int DArray_quicksort(DArray *array, DArray_compare cmp) {
 
 		// check lengths
 		check(len_less_before == DArray_count(less), "length of less changes");
+		debug("old len(more): %d; new len(more): %d", len_more_before, DArray_count(more));
 		check(len_more_before == DArray_count(more), "length of more changes");
 		
 		// at this point less and more are already sorted
 
 		// write all elements back into the old array
-		int j;
-		for(j = 0, i = 0, cur = DArray_first(less); 
-			i < DArray_max(less); 
-			j++, i++, cur = DArray_get(less, i)) {
+		for( i = 0, cur = DArray_first(less); 
+			i < DArray_count(less); 
+			i++, cur = DArray_get(less, i)) {
 
-			DArray_set(array, j, cur);
+			debug("pushing from less, length of less: %d; i: %d", DArray_count(less), i);
+			DArray_push(array, cur);
 		}
 
-		DArray_set(array, ++j, pivot);
+		DArray_push(array, pivot);
+			debug("pushing from pivot");
 
 		for(i = 0, cur = DArray_first(more); 
-			i < DArray_max(more); 
-			j++, i++, cur = DArray_get(more, i)) {
+			i < DArray_count(more); 
+			i++, cur = DArray_get(more, i)) {
 
-			DArray_set(array, j, cur);
+			debug("pushing from more");
+			DArray_push(array, cur);
 		}
 
 		// check size
@@ -115,6 +124,14 @@ int DArray_quicksort(DArray *array, DArray_compare cmp) {
 		// destroy the temporary arrays
 		DArray_destroy(less);
 		DArray_destroy(more);
+
+		// give feedback
+		#ifndef NDEBUG
+		int k;
+		for(k=0; k<DArray_count(array); k++) {
+			debug("element %d: %s", k, DArray_get(array, k));
+		}
+		#endif
 
 		return 0;
 	}
